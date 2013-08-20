509fc460b1299c6fe21beb76098d460a39a8177a
Gloton <gloton@gmail.com>
2013-08-20 14:51:20 +0200

    Con error en la vista esto es a proposito

--- model/Model.php
+++ model/Model.php
@@ -5,6 +5,14 @@ include_once("model/Book.php");
 class Model {
 	public function getBookList()
 	{
+		/*
+		jagl, ojo que si coloco en el indice, y en el primer parametro, 
+ 		texto distinto, me dará un error.
+		porque con el dato del titulo en la vista se crea un enlace 
+		(enviando parametro por get) que no lo encuentra  
+		como es el caso Moonwalker y Moonwalker2, en esta version dara error 
+		cuando pinche el enlace a Moonwalker
+		*/
 		// here goes some hardcoded values to simulate the database
 		return array(
 						"Jungle Book" => new Book("Jungle Book", "R. Kipling", "A classic book."),
